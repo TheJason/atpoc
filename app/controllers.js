d69850213ccdb064e64d3d836d1f1b3da0d70bb7
TheJason <jasonograves@gmail.com>
2014-07-22 02:32:40 -0400

    Update controllers.js

--- app/controllers.js
+++ app/controllers.js
@@ -1,6 +1,6 @@
 function PageDetailController($scope,$http,$location,$stateParams) {
 	$scope.pageid = $stateParams.pageID;
-	book.page = $scope.pageid;
+	$scope.book.page = $scope.pageid;
 	$scope.go = function ( path ) {
   		$location.path( path );
 	};
