49f47897844e58aa9099998e30f4e401998dccdf
TheJason <jasonograves@gmail.com>
2014-07-22 03:24:49 -0400

    Update controllers.js

--- app/controllers.js
+++ app/controllers.js
@@ -2,7 +2,12 @@ function PageDetailController($scope,$http,$location,$stateParams) {
 	$scope.pageid = $stateParams.pageID;
 	$scope.nextPage = $stateParams.pageID +1;
 	$scope.prevPage = $stateParams.pageID -1;
-	$scope.go = function ( path ) {
-  		$location.path( path );
-	};
+	$scope.nextPage = function(){
+	    	var url = '/page/' + $scope.nextPage;
+	    	$location.path(url);
+	}
+	$scope.prevPage = function(){
+	    	var url = '/page/' + $scope.prevPage;
+	    	$location.path(url);
+	}
 }
